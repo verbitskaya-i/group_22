13e860c32a274b736664b8c0d54aa51afcc4a02c
Irina Verbitskaya <verbitskaya789@gmail.com>
2021-09-15 22:42:27 +0300

    delete

--- Homework2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd Terminal
-mkdir folder1 folder2 folder3
-cd folder1
-touch file1.txt file2.txt file3.txt file4.json file5.json
-mkdir star1 star2 star3
-ls -la
-mv /d/courses/group_22/Terminal/folder1/{file4.json,file5.json} /d/courses/group_22/Terminal/
